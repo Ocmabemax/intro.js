40caa58cb1726ac65ceec67269853a9175dfb1f0
Ocmabemax <ocmabemax@hotmail.com>
2021-10-18 23:10:47 -0300

    Curso_clase2

---
+++ live.js
@@ -0,0 +1,21 @@
+
+// Generar una constante con la edad de una persona
+const edad1 = 17
+if (edad1 >= 18) {
+    console.log ('es mayor de edad');
+} else {
+    console.log ('es menor de edad');
+}
+
+// 1. Generar dos constantes con dos números (a y b)
+
+const a = 10
+const b = 18
+
+const suma = a + b
+if (suma >= a) {
+    console.log ('es mayor a la', suma);
+} else if (suma >= b) {
+    console.log ('es menor a la suma', suma);
+}
+
